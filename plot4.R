059dc0d454829be4f467610e7ca069a9817bff48
sha-naya <ayan.ashkenov@gmail.com>
2020-06-17 19:35:43 +0600

    Commit of the latter half of the assignment. Includes .R and .png files for plot3 and plot4.

---
+++ plot4.R
@@ -0,0 +1,21 @@
+epc.data <- read.table("/Users/Ayan/Desktop/R Specialization/ExData_Plotting1/household_power_consumption.txt", sep = ";", header = TRUE)
+epc.subset <- subset(epc.data, Date == "1/2/2007" | Date == "2/2/2007", select = c(Date, Time, Global_active_power, Global_reactive_power, Voltage, Global_intensity, Sub_metering_1, Sub_metering_2, Sub_metering_3))
+
+epc.subset <- unite_(epc.subset, "Date_Time", c("Date", "Time"))
+
+epc.subset$Date_Time <- strptime(epc.subset$Date_Time, "%d/%m/%Y_%H:%M:%S")
+
+epc.subset$Global_active_power <- as.numeric(epc.subset$Global_active_power)
+epc.subset$Global_reactive_power <- as.numeric(epc.subset$Global_reactive_power)
+epc.subset$Voltage <- as.numeric(epc.subset$Voltage)
+epc.subset$Global_intensity <- as.numeric(epc.subset$Global_intensity)
+
+with(epc.subset, {
+  plot(epc.subset$Date_Time, epc.subset$Global_active_power, type = "l", ylab = "Global Active Power", cex.lab = 0.6, cex.axis = 0.5, xlab = "")
+  plot(epc.subset$Date_Time, epc.subset$Voltage, type = "l", ylab = "Voltage", xlab = "datetime", cex.lab = 0.6, cex.axis = 0.5)
+  plot(epc.subset$Date_Time, epc.subset$Sub_metering_1, type = "l", ylab = "Energy sub metering", cex.lab = 0.6, cex.axis = 0.5, xlab = "", col = "black")
+  points(epc.subset$Date_Time, epc.subset$Sub_metering_2, type = "l", col = "red")
+  points(epc.subset$Date_Time, epc.subset$Sub_metering_3, type = "l", col = "blue")
+  legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, cex = 0.3, bty = "n")
+  plot(epc.subset$Date_Time, epc.subset$Global_reactive_power, type = "l", ylab = "Global_reactive_power", cex.lab = 0.6, cex.axis = 0.5, xlab = "datetime")
+})
